571dd0e821cab2fda32e5841e69ad2d32e8b400e
이수호 <leesuho@isuhoui-iMac.local>
2022-06-03 10:30:22 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7DpPVG7gZ1WvLBgH3-kig_HCSiyGH6iU",
+    authDomain: "sutube-578eb.firebaseapp.com",
+    projectId: "sutube-578eb",
+    storageBucket: "sutube-578eb.appspot.com",
+    messagingSenderId: "851827720779",
+    appId: "1:851827720779:web:b86f3f26b577fcb71503ce",
+    measurementId: "G-ZE1N4S1C3F"
+  };
